8f0141c0c5971a16ae1a4853d4c83ff9ef4fbeb9
Jorge Sierra <arqjorgesierra@gmail.com>
2025-06-04 22:57:06 -0500

    feat add component Hero

---
+++ components/Hero.tsx
@@ -0,0 +1,29 @@
+import {Button} from "@/components/ui/button";
+
+
+
+export default function Hero () {
+    return (
+        <section>
+            <h1 className="text-4x1 font-bold tracking-tight sm:text-5xl md:text-6xl">
+                Estudia con nosotros <br/>
+                Y se un desarrollador {" "} 
+                <span className="bg-gradient bg-gradient-to-r from-[#ff0080] to-[#7928CA] bg-clip-text text-transparent ">
+                    Full Stack
+                </span> 
+                <br></br>
+                <p className="max-w-[42rem] text-lg text-muted-foreground sm:text-xl">
+                Sumergete en el mundo de desarrollo de software, convierte el los que cambian el mundo
+                </p> 
+                <div className="flex flex-col pt-5 gap-4 sm:flex-row">
+                    <Button 
+                        variant="default"
+                        className="bg-gradient-to-r from-[#ff0080] to-[#7928CA] text-white">
+                            Comenzar
+                        </Button>
+                        
+                </div>
+            </h1>  
+        </section>
+    )
+}
